e13f8b8f02e00be1585b70d7cc3269a2527d91ce
Austin Connor <austin@Austins-MBP.home>
2022-03-25 21:02:27 -0500

    refactor Senator class

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import * as React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -8,109 +9,508 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
 import styles from '../styles/Home.module.css';
-import {Senator} from './Senator';
 
-const listOfSenators: Senator[] = [
-  new Senator('Tammy Baldwin', 'Wisconsin'),
-  new Senator('Ron Johnson', 'Wisconsin'),
-  new Senator('John Barrasso', 'Wyoming'),
-  new Senator('Cynthia M. Lummis', 'Wyoming'),
-  new Senator('Michael F. Bennett', 'Colorado'),
-  new Senator('John W. Hickenlooper', 'Colorado'),
-  new Senator('Marsha Blackburn', 'Tennessee'),
-  new Senator('Bill Hagerty', 'Tennessee'),
-  new Senator('Richard Blumenthal', 'Connecticut'),
-  new Senator('Christopher Murphy', 'Connecticut'),
-  new Senator('Roy Blunt', 'Missouri'),
-  new Senator('Josh Hawley', 'Missouri'),
-  new Senator('Cory A. Booker', 'New Jersey'),
-  new Senator('Robert Menendez', 'New Jersey'),
-  new Senator('John Boozman', 'Arkansas'),
-  new Senator('Tom Cotton', 'Arkansas'),
-  new Senator('Mike Braun', 'Indiana'),
-  new Senator('Todd Young', 'Indiana'),
-  new Senator('Sherrod Brown', 'Ohio'),
-  new Senator('Rob Portman', 'Ohio'),
-  new Senator('Richard Burr', 'North Carolina'),
-  new Senator('Thom Tillis', 'North Carolina'),
-  new Senator('Maria Cantwell', 'Washington'),
-  new Senator('Patty Murray', 'Washington'),
-  new Senator('Shelley Moore Capito', 'West Virginia'),
-  new Senator('Joe Manchin III', 'West Virginia'),
-  new Senator('Benjamin L. Cardin', 'Maryland'),
-  new Senator('Chris Van Hollen', 'Maryland'),
-  new Senator('Thomas R. Carper', 'Delaware'),
-  new Senator('Christopher A. Coons', 'Delaware'),
-  new Senator('Robert P. Casey Jr.', 'Pennsylvania'),
-  new Senator('Patrick J. Toomey', 'Pennsylvania'),
-  new Senator('Bill Cassidy', 'Louisiana'),
-  new Senator('John Kennedy', 'Louisiana'),
-  new Senator('Susan M. Collins', 'Maine'),
-  new Senator('Angus S. King Jr.', 'Maine'),
-  new Senator('John Cornyn', 'Texas'),
-  new Senator('Ted Cruz', 'Texas'),
-  new Senator('Catherine Cortez Masto', 'Nevada'),
-  new Senator('Jacky Rosen', 'Nevada'),
-  new Senator('Kevin Cramer', 'North Dakota'),
-  new Senator('John Hoeven', 'North Dakota'),
-  new Senator('Mike Crapo', 'Idaho'),
-  new Senator('James E. Risch', 'Idaho'),
-  new Senator('Steve Daines', 'Montana'),
-  new Senator('Jon Tester', 'Montana'),
-  new Senator('Tammy Duckworth', 'Illinois'),
-  new Senator('Richard J. Durbin', 'Illinois'),
-  new Senator('Joni Ernst', 'Iowa'),
-  new Senator('Chuck Grassley', 'Iowa'),
-  new Senator('Dianne Feinstein', 'California'),
-  new Senator('Alex Padilla', 'California'),
-  new Senator('Deb Fischer', 'Nebraska'),
-  new Senator('Ben Sasse', 'Nebraska'),
-  new Senator('Kirsten E. Gillibrand', 'New York'),
-  new Senator('Charles E. Schumer', 'New York'),
-  new Senator('Lindsey Graham', 'South Carolina'),
-  new Senator('Tim Scott', 'South Carolina'),
-  new Senator('Margaret Wood Hassan', 'New Hampshire'),
-  new Senator('Jeanne Shaheen', 'New Hampshire'),
-  new Senator('Martin Heinrich', 'New Mexico'),
-  new Senator('Ben Ray Luján', 'New Mexico'),
-  new Senator('Mazie K. Hirono', 'Hawaii'),
-  new Senator('Brian Schatz', 'Hawaii'),
-  new Senator('Cindy Hyde-Smith', 'Mississippi'),
-  new Senator('Roger F. Wicker', 'Mississippi'),
-  new Senator('James M. Inhofe', 'Oklahoma'),
-  new Senator('James Lankford', 'Oklahoma'),
-  new Senator('Tim Kaine', 'Virginia'),
-  new Senator('Mark R. Warner', 'Virginia'),
-  new Senator('Mark Kelly', 'Arizona'),
-  new Senator('Kyrsten Sinema', 'Arizona'),
-  new Senator('Amy Klobuchar', 'Minnesota'),
-  new Senator('Tina Smith', 'Minnesota'),
-  new Senator('Patrick J. Leahy', 'Vermont'),
-  new Senator('Bernard Sanders', 'Vermont'),
-  new Senator('Mike Lee', 'Utah'),
-  new Senator('Mitt Romney', 'Utah'),
-  new Senator('Edward J. Markey', 'Massachusetts'),
-  new Senator('Elizabeth Warren', 'Massachusetts'),
-  new Senator('Roger Marshall', 'Kansas'),
-  new Senator('Jerry Moran', 'Kansas'),
-  new Senator('Mitch McConnell', 'Kentucky'),
-  new Senator('Rand Paul', 'Kentucky'),
-  new Senator('Jeff Merkley', 'Oregon'),
-  new Senator('Ron Wyden', 'Oregon'),
-  new Senator('Lisa Murkowski', 'Alaska'),
-  new Senator('Dan Sullivan', 'Alaska'),
-  new Senator('Jon Ossoff', 'Georgia'),
-  new Senator('Raphael G. Warnock', 'Georgia'),
-  new Senator('Gary C. Peters', 'Michigan'),
-  new Senator('Debbie Stabenow', 'Michigan'),
-  new Senator('Jack Reed', 'Rhode Island'),
-  new Senator('Sheldon Whitehouse', 'Rhode Island'),
-  new Senator('Mike Rounds', 'South Dakota'),
-  new Senator('John Thune', 'South Dakota'),
-  new Senator('Marco Rubio', 'Florida'),
-  new Senator('Rick Scott', 'Florida'),
-  new Senator('Richard C. Shelby', 'Alabama'),
-  new Senator('Tommy Tuberville', 'Alabama')
+const listOfSenators = [
+  {
+    id: uuidv4(),
+    name: 'Tammy Baldwin',
+    state: 'Wisconsin'
+  },
+  {
+    id: uuidv4(),
+    name: 'Ron Johnson', 
+    state: 'Wisconsin'
+  },
+  {
+    id: uuidv4(),
+    name: 'John Barrasso',
+    state: 'Wyoming'
+  },
+  {
+    id: uuidv4(),
+    name: 'Cynthia M. Lummis',
+    state: 'Wyoming'
+  },
+  {
+    id: uuidv4(),
+    name: 'Michael F. Bennett',
+    state: 'Colorado'
+  },
+  {
+    id: uuidv4(),
+    name: 'John W. Hickenlooper',
+    state: 'Colorado'
+  },
+  {
+    id: uuidv4(),
+    name: 'Marsha Blackburn',
+    state: 'Tennessee'
+  },
+  {
+    id: uuidv4(),
+    name: 'Bill Hagerty',
+    state: 'Tennessee'
+  },
+  {
+    id: uuidv4(),
+    name: 'Richard Blumenthal',
+    state: 'Connecticut'
+  },
+  {
+    id: uuidv4(),
+    name: 'Christopher Murphy',
+    state: 'Connecticut'
+  },
+  {
+    id: uuidv4(),
+    name: 'Roy Blunt',
+    state: 'Missouri'
+  },
+  {
+    id: uuidv4(),
+    name: 'Josh Hawley',
+    state: 'Missouri'
+  },
+  {
+    id: uuidv4(),
+    name: 'Cory A. Booker',
+    state: 'New Jersey'
+  },
+  {
+    id: uuidv4(),
+    name: 'Robert Menendez',
+    state: 'New Jersey'
+  },
+  {
+    id: uuidv4(),
+    name: 'John Boozman',
+    state: 'Arkansas'
+  },
+  {
+    id: uuidv4(),
+    name: 'Tom Cotton',
+    state: 'Arkansas'
+  },
+  {
+    id: uuidv4(),
+    name: 'Mike Braun',
+    state: 'Indiana'
+  },
+  {
+    id: uuidv4(),
+    name: 'Todd Young',
+    state: 'Indiana'
+  },
+  {
+    id: uuidv4(),
+    name: 'Sherrod Brown',
+    state: 'Ohio'
+  },
+  {
+    id: uuidv4(),
+    name: 'Rob Portman',
+    state: 'Ohio'
+  },
+  {
+    id: uuidv4(),
+    name: 'Richard Burr',
+    state: 'North Carolina'
+  },
+  {
+    id: uuidv4(),
+    name: 'Thom Tillis',
+    state: 'North Carolina'
+  },
+  {
+    id: uuidv4(),
+    name: 'Maria Cantwell',
+    state: 'Washington'
+  },
+  {
+    id: uuidv4(),
+    name: 'Patty Murray',
+    state: 'Washington'
+  },
+  {
+    id: uuidv4(),
+    name: 'Shelley Moore Capito',
+    state: 'West Virginia'
+  },
+  {
+    id: uuidv4(),
+    name: 'Joe Manchin III',
+    state: 'West Virginia'
+  },
+  {
+    id: uuidv4(),
+    name: 'Benjamin L. Cardin',
+    state: 'Maryland'
+  },
+  {
+    id: uuidv4(),
+    name: 'Chris Van Hollen',
+    state: 'Maryland'
+  },
+  {
+    id: uuidv4(),
+    name: 'Thomas R. Carper',
+    state: 'Delaware'
+  },
+  {
+    id: uuidv4(),
+    name: 'Christopher A. Coons',
+    state: 'Delaware'
+  },
+  {
+    id: uuidv4(),
+    name: 'Robert P. Casey Jr.',
+    state: 'Pennsylvania'
+  },
+  {
+    id: uuidv4(),
+    name: 'Patrick J. Toomey',
+    state: 'Pennsylvania'
+  },
+  {
+    id: uuidv4(),
+    name: 'Bill Cassidy',
+    state: 'Louisiana'
+  },
+  {
+    id: uuidv4(),
+    name: 'John Kennedy',
+    state: 'Louisiana'
+  },
+  {
+    id: uuidv4(),
+    name: 'Susan M. Collins',
+    state: 'Maine'
+  },
+  {
+    id: uuidv4(),
+    name: 'Angus S. King Jr.',
+    state: 'Maine'
+  },
+  {
+    id: uuidv4(),
+    name: 'John Cornyn',
+    state: 'Texas'
+  },
+  {
+    id: uuidv4(),
+    name: 'Ted Cruz',
+    state: 'Texas'
+  },
+  {
+    id: uuidv4(),
+    name: 'Catherine Cortez Masto',
+    state: 'Nevada'
+  },
+  {
+    id: uuidv4(),
+    name: 'Jacky Rosen',
+    state: 'Nevada'
+  },
+  {
+    id: uuidv4(),
+    name: 'Kevin Cramer',
+    state: 'North Dakota'
+  },
+  {
+    id: uuidv4(),
+    name: 'John Hoeven',
+    state: 'North Dakota'
+  },
+  {
+    id: uuidv4(),
+    name: 'Mike Crapo',
+    state: 'Idaho'
+  },
+  {
+    id: uuidv4(),
+    name: 'James E. Risch',
+    state: 'Idaho'
+  },
+  {
+    id: uuidv4(),
+    name: 'Steve Daines',
+    state: 'Montana'
+  },
+  {
+    id: uuidv4(),
+    name: 'Jon Tester',
+    state: 'Montana'
+  },
+  {
+    id: uuidv4(),
+    name: 'Tammy Duckworth',
+    state: 'Illinois'
+  },
+  {
+    id: uuidv4(),
+    name: 'Richard J. Durbin',
+    state: 'Illinois'
+  },
+  {
+    id: uuidv4(),
+    name: 'Joni Ernst',
+    state: 'Iowa'
+  },
+  {
+    id: uuidv4(),
+    name: 'Chuck Grassley',
+    state: 'Iowa'
+  },
+  {
+    id: uuidv4(),
+    name: 'Dianne Feinstein',
+    state: 'California'
+  },
+  {
+    id: uuidv4(),
+    name: 'Alex Padilla',
+    state: 'California'
+  },
+  {
+    id: uuidv4(),
+    name: 'Deb Fischer',
+    state: 'Nebraska'
+  },
+  {
+    id: uuidv4(),
+    name: 'Ben Sasse',
+    state: 'Nebraska'
+  },
+  {
+    id: uuidv4(),
+    name: 'Kirsten E. Gillibrand',
+    state: 'New York'
+  },
+  {
+    id: uuidv4(),
+    name: 'Charles E. Schumer',
+    state: 'New York'
+  },
+  {
+    id: uuidv4(),
+    name: 'Lindsey Graham',
+    state: 'South Carolina'
+  },
+  {
+    id: uuidv4(),
+    name: 'Tim Scott',
+    state: 'South Carolina'
+  },
+  {
+    id: uuidv4(),
+    name: 'Margaret Wood Hassan',
+    state: 'New Hampshire'
+  },
+  {
+    id: uuidv4(),
+    name: 'Jeanne Shaheen',
+    state: 'New Hampshire'
+  },
+  {
+    id: uuidv4(),
+    name: 'Martin Heinrich',
+    state: 'New Mexico'
+  },
+  {
+    id: uuidv4(),
+    name: 'Ben Ray Luján',
+    state: 'New Mexico'
+  },
+  {
+    id: uuidv4(),
+    name: 'Mazie K. Hirono',
+    state: 'Hawaii'
+  },
+  {
+    id: uuidv4(),
+    name: 'Brian Schatz',
+    state: 'Hawaii'
+  },
+  {
+    id: uuidv4(),
+    name: 'Cindy Hyde-Smith',
+    state: 'Mississippi'
+  },
+  {
+    id: uuidv4(),
+    name: 'Roger F. Wicker',
+    state: 'Mississippi'
+  },
+  {
+    id: uuidv4(),
+    name: 'James M. Inhofe',
+    state: 'Oklahoma'
+  },
+  {
+    id: uuidv4(),
+    name: 'James Lankford',
+    state: 'Oklahoma'
+  },
+  {
+    id: uuidv4(),
+    name: 'Tim Kaine',
+    state: 'Virginia'
+  },
+  {
+    id: uuidv4(),
+    name: 'Mark R. Warner',
+    state: 'Virginia'
+  },
+  {
+    id: uuidv4(),
+    name: 'Mark Kelly',
+    state: 'Arizona'
+  },
+  {
+    id: uuidv4(),
+    name: 'Kyrsten Sinema',
+    state: 'Arizona'
+  },
+  {
+    id: uuidv4(),
+    name: 'Amy Klobuchar',
+    state: 'Minnesota'
+  },
+  {
+    id: uuidv4(),
+    name: 'Tina Smith',
+    state: 'Minnesota'
+  },
+  {
+    id: uuidv4(),
+    name: 'Patrick J. Leahy',
+    state: 'Vermont'
+  },
+  {
+    id: uuidv4(),
+    name: 'Bernard Sanders',
+    state: 'Vermont'
+  },
+  {
+    id: uuidv4(),
+    name: 'Mike Lee',
+    state: 'Utah'
+  },
+  {
+    id: uuidv4(),
+    name: 'Mitt Romney',
+    state: 'Utah'
+  },
+  {
+    id: uuidv4(),
+    name: 'Edward J. Markey',
+    state: 'Massachusetts'
+  },
+  {
+    id: uuidv4(),
+    name: 'Elizabeth Warren',
+    state: 'Massachusetts'
+  },
+  {
+    id: uuidv4(),
+    name: 'Roger Marshall',
+    state: 'Kansas'
+  },
+  {
+    id: uuidv4(),
+    name: 'Jerry Moran',
+    state: 'Kansas'
+  },
+  {
+    id: uuidv4(),
+    name: 'Mitch McConnell',
+    state: 'Kentucky'
+  },
+  {
+    id: uuidv4(),
+    name: 'Rand Paul',
+    state: 'Kentucky'
+  },
+  {
+    id: uuidv4(),
+    name: 'Jeff Merkley',
+    state: 'Oregon'
+  },
+  {
+    id: uuidv4(),
+    name: 'Ron Wyden',
+    state: 'Oregon'
+  },
+  {
+    id: uuidv4(),
+    name: 'Lisa Murkowski',
+    state: 'Alaska'
+  },
+  {
+    id: uuidv4(),
+    name: 'Dan Sullivan',
+    state: 'Alaska'
+  },
+  {
+    id: uuidv4(),
+    name: 'Jon Ossoff',
+    state: 'Georgia'
+  },
+  {
+    id: uuidv4(),
+    name: 'Raphael G. Warnock',
+    state: 'Georgia'
+  },
+  {
+    id: uuidv4(),
+    name: 'Gary C. Peters',
+    state: 'Michigan'
+  },
+  {
+    id: uuidv4(),
+    name: 'Debbie Stabenow',
+    state: 'Michigan'
+  },
+  {
+    id: uuidv4(),
+    name: 'Jack Reed',
+    state: 'Rhode Island'
+  },
+  {
+    id: uuidv4(),
+    name: 'Sheldon Whitehouse',
+    state: 'Rhode Island'
+  },
+  {
+    id: uuidv4(),
+    name: 'Mike Rounds',
+    state: 'South Dakota'
+  },
+  {
+    id: uuidv4(),
+    name: 'John Thune',
+    state: 'South Dakota'
+  },
+  {
+    id: uuidv4(),
+    name: 'Marco Rubio',
+    state: 'Florida'
+  },
+  {
+    id: uuidv4(),
+    name: 'Rick Scott',
+    state: 'Florida'
+  },
+  {
+    id: uuidv4(),
+    name: 'Richard C. Shelby',
+    state: 'Alabama'
+  },
+  {
+    id: uuidv4(),
+    name: 'Tommy Tuberville',
+    state: 'Alabama'
+  }
 ]
 
 const BasicList = () => {
@@ -118,7 +518,7 @@ const BasicList = () => {
     <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       <nav aria-label="main mailbox folders">
         <List>
-          {listOfSenators.map((senator: Senator) =>
+          {listOfSenators.map((senator) =>
             <ListItem disablePadding key={senator.id}>
               <ListItemButton component="a" href="#simple-list">
                 <ListItemText primary={`${senator.name} - ${senator.state}`} />
